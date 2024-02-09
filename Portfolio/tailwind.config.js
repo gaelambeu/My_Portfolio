4aaf9e7c1451a18f7c1f3d0acdfc3d9a1a457c64
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-09 00:06:00 +0000

    short

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -5,6 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily:{
+      primary: 'Orbitron',
+      secondary: 'Rajdhani',
+      tertiary: 'Aldrich',
+    },
+    container:{
+      padding:{
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
     extend: {},
   },
   plugins: [],
