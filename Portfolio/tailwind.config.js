2bba246af42d7dab3b306ed60c80e6fd2f0c0112
GAEL AMBEU <ambeugael128@gmail.com>
2024-02-09 00:25:17 +0000

    short

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -1,36 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily:{
-      primary: 'Orbitron',
-      secondary: 'Rajdhani',
-      tertiary: 'Aldrich',
-    },
-    container:{
-      padding:{
-        DEFAULT: '15px',
-      },
-    },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px',
-    },
     extend: {
-      colors: {
-        primary: '#0a0a0a',
-        accent: '#8809C3',
-      },
       backgroundImage: {
-        
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-
+};
