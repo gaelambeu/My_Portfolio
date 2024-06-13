d43cd90c34673f69fa533ade70d5dcebae62abaf
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-13 12:26:42 +0300

    shorrt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <header className="header">
-        <NavLink to="/" className="w-17 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <NavLink to="/" className="w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
           <p className="blue-gradient_text">A3EG</p>
         </NavLink>
 
