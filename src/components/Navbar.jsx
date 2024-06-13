9aba4a84130a66b14a5c298305d6b81d4c96c4bc
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-13 12:05:14 +0300

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar Ambeu</div>
+    <header className="header">
+        <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+          <p className="blue-gradient_text">AAAEG</p>
+        </NavLink>
+
+        <nav className="flex text-lg gap-7 font-medium">
+          <NavLink to="/abour">
+            About
+          </NavLink>
+        </nav>
+    </header>
   )
 }
 
