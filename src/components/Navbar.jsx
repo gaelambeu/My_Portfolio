913a0fb68a1da91f0ce2fad939e393633442fa6b
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-19 21:08:09 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,16 @@ const Navbar = () => {
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
-        
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="" className="object-contain w-9 h-9" />
+        </Link>
       </div>
     </nav>
   )
