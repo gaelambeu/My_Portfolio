cb880489b9783ec74f0f7b214b567edde4df80d9
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-19 20:36:00 +0300

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { BrowserRouter } from "react-router-dom"
+import {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+} from './components'
+
 const App = () => {
 
   return (
-    <div>
-      3D Developer Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-center bg-no-repeat bg-cover bg-hero-pattern">
+          <Navbar />
+          <Hero />
+          <div>
+            <About />
+            <Experience />
+            <Tech />
+            <Works />  
+            <Feedbacks />  
+            <div className="relative z-0">
+              <Contact />  
+              <StarsCanvas />   
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
