01735397479c996b1c58c6f1c7dd474e03b6e4f2
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-20 17:34:24 +0300

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="" className="object-contain w-9 h-9" />
-          <p className="text-white text-[18px] flex font-bold cursor-pointer">
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
             AMBEU 
             <span className="hidden sm:block">
               | <AEG coding />
