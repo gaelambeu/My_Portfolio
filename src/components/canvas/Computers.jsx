e9d85b5e8dcc3279b546130676084c1c3fa29957
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-20 18:35:26 +0300

    computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,16 @@
 import {Suspense, useEffect, useState} from 'react'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, meshBounds, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from "../Loader"
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gif')
+
   return (
-    <div>Computers transform</div>
+    <mesh>
+      
+    </mesh>
   )
 }
 
